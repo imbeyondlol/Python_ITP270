89d3db4e66655c66674f36bddcec1ebead9aefbf
Cyber Range Student <student@ip-10-1-150-98.ec2.internal>
2023-04-28 02:33:14 +0000

    Thread

--- Physics.py
+++ Physics.py
@@ -1,4 +1,4 @@
-#!bin/python3
+#!/bin/python3
 
 train_mass = 22680
 
---
+++ Thread_Shed.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+
+daily_sales = "Edith Mcbride" ;,;$1.21 ;,; white ;,; 09/15/17 ,
+
+"Herbert Tran";,; $7.29;,;white&blue;,; 09/15/17 ,
+
+"Paul Clarke";,;$12.52 ;,; white&blue ;,; 09/15/17 ,
+
+"Lucille Caldwell";,; $5.13 ;,; white ;,; 09/15/17,
+
+"Eduardo George";,;$20.39;,; white&yellow ;,;09/15/17 , 
+
+"Danny Mclaughlin";,;$30.82;,; purple ;,;09/15/17 ,
+
+"Stacy Vargas";,; $1.85 ;,; purple&yellow ;,;09/15/17,
+
+"Shaun Brock";,;$17.98;,;purple&yellow ;,; 09/15/17 ,
+
+"Erick Harper";,;$17.41;,; blue ;,; 09/15/17,
+
+"Michelle Howell";,;$28.59;,; blue;,; 09/15/17 ,
+
+"Carroll Boyd";,; $14.51;,; purple&blue ;,;09/15/17 ,
+
+"Teresa Carter";,; $19.64 ;,; white;,;09/15/17 ,
+
+"Jacob Kennedy";,; $11.40 ;,; white&red ;,; 09/15/17,
+
+"Craig Chambers";,;$8.79 ;,; white&blue&red ;,;09/15/17 ,
+
+"Peggy Bell";,; $8.65 ;,;blue ;,; 09/15/17,
+
+"Kenneth Cunningham";,; $10.53;,; green&blue ;,;09/15/17 ,
+
+"Marvin Morgan";,; $16.49;,;green&blue&red ;,; 09/15/17 ,
+
+"Marjorie Russell";,; $6.55 ;,; green&blue&red;,; 09/15/17 ,
+
+"Israel Cummings";,; $11.86 ;,;black;,;09/15/17,
+
+"June Doyle";,; $22.29 ;,;black&yellow ;,;09/15/17 ,
+
+"Jaime Buchanan";,;$8.35;,; white&black&yellow ;,; 09/15/17,
+
+"Rhonda Farmer";,;$2.91 ;,; white&black&yellow ;,;09/15/17,
+
+"Darren Mckenzie" ;,;$22.94;,;green;,;09/15/17,
+
+"Rufus Malone";,;$4.70 ;,; green&yellow ;,;09/15/17 ,
+
+"Hubert Miles";,;$3.59 ;,;green&yellow&blue;,; 09/15/17 ,
+
+"Joseph Bridges";,;$5.66 ;,; green&yellow&purple&blue ;,; 09/15/17 ,
+
+
